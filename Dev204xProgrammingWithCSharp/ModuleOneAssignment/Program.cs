aaa35a310c97aac38555a52d08b6058796426335
André Davis <cubicle.jockey@live.com>
2015-04-09 21:03:35 -0700

    Bored, going to play video games

--- Dev204xProgrammingWithCSharp/ModuleOneAssignment/Program.cs
+++ Dev204xProgrammingWithCSharp/ModuleOneAssignment/Program.cs
@@ -1,13 +1,49 @@
-﻿namespace ModuleOneAssignment
+﻿using System;
+
+namespace ModuleOneAssignment
 {
-    class Program
+    /// <summary>
+    /// Dev204x Programming with C#
+    /// This is assignment 1 for Module 1
+    /// </summary>
+    public class Program
     {
         public static void Main(string[] args)
         {
             #region Student Variables
 
+            string studentFirstName = "André";
+            string studentLastName = "Davis";
+            DateTime studentBirthday = new DateTime(1982, 11, 11);
+            string studentAddress = "1234 Awesome Ln Blvd";
+            string studentAddress1 = "Apt #123";
+            string studentCity = "Seattle";
+            string studentState = "WA";
+            string studentZip = "98102-6249";
+            string studentCountry = "USA";
+            string sutdentNotes = "Dis student be jammin'";
+
 
             #endregion Student Variables
+
+            #region Professor Variables
+
+            string professorPreFix = "Dr.";
+            string professorFirstName = "Adam";
+            string professorLastName = "West";
+            string professorDepartment = "Sustainable Food Systems and Permaculture Design";
+            
+
+            #endregion Professor Variables
+
+            #region University Degree Variables
+
+            string universityDegreeLevel = "Masters";
+            string univeristyDegreeMajor = "Permaculture";
+            short univerisityDegreeRequiredCredits = 160;
+
+
+            #endregion Univeristy Degree Variables
         }
     }
 }
