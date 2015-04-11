53c88f653189a706e63c7ff1830afb16e41bb62d
André Davis <cubicle.jockey@live.com>
2015-04-11 15:19:47 -0700

    Finished the homework

--- Dev204xProgrammingWithCSharp/ModuleOneAssignment/Program.cs
+++ Dev204xProgrammingWithCSharp/ModuleOneAssignment/Program.cs
@@ -21,7 +21,7 @@ namespace ModuleOneAssignment
             string studentState = "WA";
             string studentZip = "98102-6249";
             string studentCountry = "USA";
-            string sutdentNotes = "Dis student be jammin'";
+            string studentNotes = "Dis student be jammin'";
 
 
             #endregion Student Variables
@@ -39,11 +39,79 @@ namespace ModuleOneAssignment
             #region University Degree Variables
 
             string universityDegreeLevel = "Masters";
-            string univeristyDegreeMajor = "Permaculture";
+            string univeristyDegreeMajor = "Permaculture Design and Sustainable Food Systems";
             short univerisityDegreeRequiredCredits = 160;
 
-
             #endregion Univeristy Degree Variables
+
+            #region University Program Variables
+
+            string univserityProgramName = "Whole Systems Design";
+            
+            //Doing this as array to give a preview
+            string[] universityDegreesOffered = new[] {"Bachelor", "Masters", "Ph.d" };
+            
+            string univeristyProgramDepartmentHead = "Batman";
+
+            #endregion University Program Variables
+
+            #region A Course Variables
+
+            string courseCode = "PC101";
+            string courseTitle = "Introduction To Permaculture Design";
+            short courseCreditHours = 5;
+            string courseDescription = "This is an introductory course to learn the basic Prinicples that are the foundation of Permaculture.";
+
+            #endregion A Course Variables
+
+            #region Display to Console
+
+            Console.WriteLine("Student Information");
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+            Console.WriteLine("First Name: {0}", studentFirstName);
+            Console.WriteLine("Last Name: {0}", studentLastName);
+            Console.WriteLine("Birthday: {0}", studentBirthday.ToShortDateString());
+            Console.WriteLine("Address: {0}", studentAddress);
+            Console.WriteLine("Address1: {0}", studentAddress1);
+            Console.WriteLine("City: {0}", studentCity);
+            Console.WriteLine("State: {0}", studentState);
+            Console.WriteLine("Country: {0}", studentCountry);
+            Console.WriteLine("Notes: {0}", studentNotes);
+            Console.WriteLine();
+            Console.WriteLine("Professor");
+            Console.WriteLine("---------");
+            Console.WriteLine("Prefix: {0}", professorPreFix);
+            Console.WriteLine("First Name: {0}", professorFirstName);
+            Console.WriteLine("Last Name: {0}", professorLastName);
+            Console.WriteLine("Department: {0}", professorDepartment);
+            Console.WriteLine();
+            Console.WriteLine("Degree");
+            Console.WriteLine("------");
+            Console.WriteLine("Level: {0}", universityDegreeLevel);
+            Console.WriteLine("Major: {0}", univeristyDegreeMajor);
+            Console.WriteLine("Credit Hours Required: {0}", univerisityDegreeRequiredCredits);
+            Console.WriteLine();
+            Console.WriteLine("Program");
+            Console.WriteLine("-------");
+            Console.WriteLine("Name: {0}", univserityProgramName);
+            Console.WriteLine("Department Head: {0}", univeristyProgramDepartmentHead);
+            Console.WriteLine("Degree's offered:");
+            Console.WriteLine("\t{0}", universityDegreesOffered[0]);
+            Console.WriteLine("\t{0}", universityDegreesOffered[1]);
+            Console.WriteLine("\t{0}", universityDegreesOffered[2]);
+            Console.WriteLine();
+            Console.WriteLine("Current Course");
+            Console.WriteLine("--------------");
+            Console.WriteLine("Code: {0}", courseCode);
+            Console.WriteLine("Title: {0}", courseTitle);
+            Console.WriteLine("Credit Hours: {0}", courseCreditHours);
+            Console.WriteLine("Description: {0}", courseDescription);
+
+            #endregion Display to Console
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
         }
     }
 }
---
+++ Dev204xProgrammingWithCSharp/ModuleTwoAssignment/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+
+namespace ModuleTwoAssignment
+{
+    /// <summary>
+    /// This is Homework assignment 2 for Module Two
+    /// 
+    /// We are to use loop and conditional logic to produce the below output:
+    /// 
+    /// XOXOXOXO
+    /// OXOXOXOX
+    /// XOXOXOXO
+    /// OXOXOXOX
+    /// XOXOXOXO
+    /// OXOXOXOX
+    /// XOXOXOXO
+    /// OXOXOXOX
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Constant Values
+
+            /* 
+             * 'const' means these values cannot change
+             * Documentation here: https://msdn.microsoft.com/en-us/library/e6w8fe1b.aspx
+             */
+            const int TOTALGRIDITEMS = 64; //8x8 structure
+            const int ROWLENGTH = 8;
+            const string X = "X";
+            const string O = "O";
+
+            #endregion Constant Values
+
+            /*These built-in C# objects are memory efficient ways to deal with lots of 
+             * string contacts aka + overloaded operands or string.concat() method calls
+             * 
+             * Documentation: https://msdn.microsoft.com/en-us/library/system.text.stringbuilder%28v=vs.110%29.aspx
+             */
+            var output = new StringBuilder();
+            var currentLine = new StringBuilder();
+
+            int numLines = 0;
+            for (var i = 0; i <= TOTALGRIDITEMS; i++)
+            {
+                if ((numLines%2) == 0)
+                {
+                    /*
+                       ((i % 2) != 0 ? O : X) is an if/else done in it's operator format
+                       Documentation here: https://msdn.microsoft.com/en-us/library/ty67wk28.aspx
+                     
+                        It's the same as writing the code this way:
+                        if ((i%2) == 0)
+                        {
+                            currentLine.Append(O);
+                        }
+                        else
+                        {
+                            currentLine.Append(X);
+                        }
+                     */
+                    currentLine.Append((i % 2) != 0 ? O : X);
+                }
+                else
+                {
+                    currentLine.Append((i % 2) == 0 ? O : X);
+                }
+
+                if (currentLine.Length == ROWLENGTH)
+                {
+                    output.AppendLine(currentLine.ToString());
+                    currentLine.Clear();
+                    numLines++;
+                }
+            }
+
+            Console.WriteLine(output.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+
+            /*
+               If you would like to be able to run this code and test it quickly without using visual studio
+               you can download LINQPad at https://www.linqpad.net/. Once downloaded open LINQPad change the 
+               language drop down to C# Program, delete the void Main() and paste this code in there. Press
+               the play button and you will get the output. LINQPad is a great way to try out snippets of code
+               without having to build an entire project within Visual Studio.
+             
+               I hope who ever grades this tries out this wonderful tool to help your learning.
+             */
+        }
+    }
+}
