3f93c4d2805d2ce3a30ad05fd62a2affa0b86700
André Davis <cubicle.jockey@live.com>
2015-04-12 12:02:14 -0700

    Done with lectures for Module 3 homework not yet available

---
+++ Dev204xProgrammingWithCSharp/ModuleThree/Exceptions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModuleThree
+{
+    [TestClass]
+    public class Exceptions
+    {
+        [TestMethod]
+        public void SafeDivision_Valid()
+        {
+            try
+            {
+                const double lhs = 5;
+                const double rhs = 10;
+
+                double result = SafeDivision(lhs, rhs);
+                Console.WriteLine("{0} divided by {1} = {2}", lhs, rhs, result);
+            }
+            catch (DivideByZeroException dbzx)
+            {
+                //We will not get here or see this message
+                Console.WriteLine(dbzx.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SafeDivision_DivideByZero()
+        {
+            try
+            {
+                const double lhs = 67;
+                const double rhs = 0;
+
+                double result = SafeDivision(lhs, rhs);
+                Console.WriteLine("We will not see this message");
+            }
+            catch (DivideByZeroException dbzx)
+            {
+                Console.WriteLine(dbzx.Message);
+            }
+        }
+
+        #region Helper Methods
+
+        public static double SafeDivision(double lhs, double rhs)
+        {
+            if (rhs == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return (lhs/rhs);
+        }
+
+        #endregion Helper Methods
+    }
+}
