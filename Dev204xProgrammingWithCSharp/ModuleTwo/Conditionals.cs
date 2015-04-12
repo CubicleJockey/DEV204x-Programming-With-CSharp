961ec8a6b4e22cc3f04132eaa6ae123df87945ee
André Davis <cubicle.jockey@live.com>
2015-04-12 11:15:21 -0700

    Examples for Module 2 concepts added

---
+++ Dev204xProgrammingWithCSharp/ModuleTwo/Conditionals.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModuleTwo
+{
+    [TestClass]
+    public class Conditionals
+    {
+        private readonly Random _random = new Random();
+
+        [TestMethod]
+        public void If()
+        {
+            //if(condition is true)
+
+            bool condition = true;
+            if (condition)
+            {
+                Console.WriteLine("Condition was met you will see this message in your output");
+            }
+
+            bool condition2 = false;
+            if (condition2)
+            {
+                Console.WriteLine("Condition was NOT net you will NOT see this message in your output");
+            }
+
+        }
+
+        [TestMethod]
+        public void IfElse()
+        {
+            const string NAME = "CubicleJockey";
+
+            if (NAME == "Batman")
+            {
+                Console.WriteLine("This person is obviously Batman");
+            }
+            else
+            {
+                Console.WriteLine("This person is obviously NOT Batman");
+            }
+        }
+
+        [TestMethod]
+        public void IfElseIfElse()
+        {
+            //get a random number with a max of 3
+            var number = _random.Next(3);
+
+            if (number == 1)
+            {
+                Console.WriteLine("Number was One");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("Number was Two");
+            }
+            else
+            {
+                Console.WriteLine("Number was Three");
+            }
+        }
+
+        [TestMethod]
+        public void IfElseOperator()
+        {
+            //This is exactly the same ability as a regular if/else it's just written in operator form
+            bool randomBoolean = (_random.NextDouble() >= 0.5);
+
+            //? = if and : = else
+            var result = randomBoolean ? "Was True" : "Was False"; 
+            Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public void SwitchStatement()
+        {
+
+            var number = _random.Next(4);
+            switch (number)
+            {
+                case 0:
+                    Console.WriteLine("Number was Zero");
+                    break;
+                case 1:
+                    Console.WriteLine("Number was One");
+                    break;
+                case 2:
+                    Console.WriteLine("Number was Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Number was Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Number was Four");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(string.Format("Unexpected value was generated. {0}", number));
+            }
+        }
+    }
+}
---
+++ Dev204xProgrammingWithCSharp/ModuleTwo/Loops.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModuleTwo
+{
+    [TestClass]
+    public class Loops
+    {
+        [TestMethod]
+        public void ForLoop()
+        {
+            //an array of numbers 1 through 100
+            var numbers = Enumerable.Range(1, 100).ToArray();
+
+            //loop over each item in the numbers array
+            for (var index = 0; index < numbers.Length; index++)
+            {
+                //print out the value to the screen
+                Console.WriteLine(numbers[index]);
+            }
+        }
+
+        [TestMethod]
+        public void ForeachLoop()
+        {
+            //an IEnumerable<int> loopable like an array or list, because array and list both inherit from iEnumerable<T>
+            //That is just something to note for when we get into classes
+            var numbers = Enumerable.Range(1, 100);
+
+            //loop over each item in the enumerable of integers
+            foreach (var number in numbers)
+            {
+                //print out the value to the screen
+                Console.WriteLine(number);
+            }
+        }
+
+        [TestMethod]
+        public void WhileLoop()
+        {
+            //Setting up things a little different to expose to different
+            //ways of doing the same thing
+
+            const int MAXNUMBER = 100;
+            var numbers = Enumerable.Range(1, MAXNUMBER);
+            var currentNumber = 0;
+
+            while (currentNumber < MAXNUMBER)
+            {
+                //print out the value to the screen
+                //.ElementAt(int) is an extension method, syntactic sugar
+                //Documentation here: https://msdn.microsoft.com/en-us/library/bb383977.aspx
+                Console.WriteLine(numbers.ElementAt(currentNumber++));
+
+                /*
+                 * Incrementing numbers with the ++ or Decrementing with the -- operator
+                 * 
+                 * If the incrementor/decrementor is in front of the number like this
+                 * ++currentNumber or --currentNumber this is saying increase or decrease value before using it
+                 * 
+                 * If the incrementor/decrementor is behind the number like this
+                 * currentNumber++ or currentNumber-- it means use the value then after increase or decrease the value
+                 */
+            }
+
+        }
+
+        [TestMethod]
+        public void DoWhileLoop()
+        {
+            var numbers = Enumerable.Range(1, 100).ToList();
+
+            //do-while loops will ALWAYS run at least once
+            do
+            {
+                //indexes always start at 0, off by one if you only use count
+                //Also in this instance different from the other loops
+                //Lets print the numbers out biggest to smallest
+                var index = (numbers.Count - 1);
+                var number = numbers[index];
+                numbers.RemoveAt(index);
+
+                //display number on the screen
+                Console.WriteLine(number);
+            } while (numbers.Count > 0);
+
+        }
+    }
+}
