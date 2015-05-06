62b3b8fd9834b4ff0da84c5745075e6da938dd48
André Davis <CubicleJockey@users.noreply.github.com>
2015-05-06 11:52:24 -0700

    Finished Module 9
    No small example section use the homework

---
+++ Dev204xProgrammingWithCSharp/ModuleNine/EventsAndDelegates.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModuleNine
+{
+    [TestClass]
+    public class EventsAndDelegates
+    {
+        [TestMethod]
+        public void Main()
+        {
+            //See homework for Module 9
+        }
+    }
+}
---
+++ Dev204xProgrammingWithCSharp/ModuleNineHomework/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Mod_9_Homework
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
