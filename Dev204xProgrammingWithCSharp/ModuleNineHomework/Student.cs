6718e7ae859c3ea59fe93d3ad33940b0f290de29
André Davis <CubicleJockey@users.noreply.github.com>
2015-05-06 12:14:59 -0700

    Git forgot to add Student class

---
+++ Dev204xProgrammingWithCSharp/ModuleNineHomework/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Mod_9_Homework
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
